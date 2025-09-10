e153adb496d894dff6820f7cf6076e866f04d7b4
@SentinelTusk <luceroluisfrancisco2@gmail.com>
2025-09-10 14:43:25 +0800

    driver class

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-import os
-from flask import Flask, request, jsonify, render_template
 
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, loginUser, loginRequired, logoutUser, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import math
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ambulance_tracker.db"
+app.config["SECRET_KEY"] = os.urandom(24)
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Ambulance Driver account
+Class Driver(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 @app.route('/')
 def home():
