8d2f802ed0b0065db32407a7bbca19618a150ee3
@SentinelTusk <luceroluisfrancisco2@gmail.com>
2025-09-10 13:03:43 +0800

    setup 2

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request, jsonify, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
